5b5dd19213705b619c4b2c2b38e794a97717b9d8
HarshIyer <git@hi05.xyz>
2024-06-26 21:01:41 +0530

    Fix image import and add favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
+import logo from "../assets/Logo.png";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
@@ -7,11 +7,7 @@ const Navbar = () => {
       <div className="md:bg-[#0D0D0D] rounded-3xl p-3 w-fit">
         <div className="flex flex-col md:flex-row items-center">
           <div className="mb-2 md:mb-0 flex flex-row items-center">
-            <img
-              src="src/assets/Logo.png"
-              alt="logo"
-              className="ml-4 w-12 h-12"
-            />
+            <img src={logo} alt="logo" className="ml-4 w-12 h-12" />
             <h1 className="text-xl text-white font-black ml-2 md:ml-6 md:mr-16 items-center">
               Proximus
             </h1>
