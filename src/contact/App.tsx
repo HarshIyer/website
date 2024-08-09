16a01e6517514a21819cf4e69660ef6108accb9d
HarshIyer <git@hi05.xyz>
2024-08-09 11:01:08 +0530

    Add pgp

--- src/contact/App.tsx
+++ src/contact/App.tsx
@@ -16,13 +16,13 @@ const ContactPage = () => {
       <div>
         <h1
           className={
-            w-fit green-colored text-3xl font-extrabold ml-8 text-center md:ml-[15%] mt-8 +
+            `w-fit green-colored text-3xl font-extrabold ml-8 text-center md:ml-[15%] mt-8` +
             (isInView ? " reveal-animation" : "")
           }
         >
           Contact
         </h1>
-        <div className={flex flex-col + isInView ? " text-fade" : ""}>
+        <div className={`flex flex-col` + isInView ? " text-fade" : ""}>
           <p className="text-white text-xl font-semibold mx-8 md:ml-[20%] mt-8">
             You can send me an email at{" "}
             <span className="green-colored">hello at harshiyer dot me</span>
@@ -68,19 +68,19 @@ const ContactPage = () => {
             <p>My PGP key</p>
           </div>
           <div className="  ">
-            <pre className="overflow-x-scroll md:ml-[20%] bg-[#575757] mx-8 text-white  rounded-lg p-2  mt-4">
+            <pre className="overflow-x-scroll md:ml-[20%] w-fit p-4 bg-[#575757] mx-8 text-white  rounded-lg p-2  mt-4">
               {`-----BEGIN PGP PUBLIC KEY BLOCK-----
 
-              mDMEZrUQLRYJKwYBBAHaRw8BAQdAmVmtWaBxs+cSZfV5NjESlG3Oduy99n0/HG+3
-              E7D5gHa0HXByb3hpbXVzIDxoZWxsb0BoYXJzaGl5ZXIubWU+iJkEExYKAEEWIQQf
-              zOifLLzbMfPucqxR1Ahykd6HYQUCZrUQLQIbAwUJBaOagAULCQgHAgIiAgYVCgkI
-              CwIEFgIDAQIeBwIXgAAKCRBR1Ahykd6HYcVJAP46aKr54SYAxzJ6LGZrJysqBsnH
-              BaU6Eywh1jpLGF6otQD+KSgQCHl1GvR+8agAadOGcE/8Pv18+GY6uhkD+WKfKAO4
-              OARmtRAtEgorBgEEAZdVAQUBAQdA5EmbGbCQKFgWmzmUk+S9oVuk2Y916rbO6Y/3
-              eYewligDAQgHiH4EGBYKACYWIQQfzOifLLzbMfPucqxR1Ahykd6HYQUCZrUQLQIb
-              DAUJBaOagAAKCRBR1Ahykd6HYWaXAP95XVhlsWNAfvWi9fUY/MzTba02QM2/AkJP
-              Wl0tZByHCQEApl6Jjx6X9WeQf/F91g/bwRmWYV2DE6TmALQgnNhKdQM=
-              =QSzq
+mDMEZrUQLRYJKwYBBAHaRw8BAQdAmVmtWaBxs+cSZfV5NjESlG3Oduy99n0/HG+3
+E7D5gHa0HXByb3hpbXVzIDxoZWxsb0BoYXJzaGl5ZXIubWU+iJkEExYKAEEWIQQf
+zOifLLzbMfPucqxR1Ahykd6HYQUCZrUQLQIbAwUJBaOagAULCQgHAgIiAgYVCgkI
+CwIEFgIDAQIeBwIXgAAKCRBR1Ahykd6HYcVJAP46aKr54SYAxzJ6LGZrJysqBsnH
+BaU6Eywh1jpLGF6otQD+KSgQCHl1GvR+8agAadOGcE/8Pv18+GY6uhkD+WKfKAO4
+OARmtRAtEgorBgEEAZdVAQUBAQdA5EmbGbCQKFgWmzmUk+S9oVuk2Y916rbO6Y/3
+eYewligDAQgHiH4EGBYKACYWIQQfzOifLLzbMfPucqxR1Ahykd6HYQUCZrUQLQIb
+DAUJBaOagAAKCRBR1Ahykd6HYWaXAP95XVhlsWNAfvWi9fUY/MzTba02QM2/AkJP
+Wl0tZByHCQEApl6Jjx6X9WeQf/F91g/bwRmWYV2DE6TmALQgnNhKdQM=
+=QSzq
 
 -----END PGP PUBLIC KEY BLOCK-----`}
             </pre>
